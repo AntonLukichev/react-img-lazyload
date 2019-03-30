22e295f24d940f67cdba2750e99f1084ad24a746
Anton Lukichev <anton@lukichev.pro>
2019-03-30 07:41:45 +0300

    test DPR

--- README.md
+++ README.md
@@ -21,11 +21,13 @@ npm i react-img-lazyload --save
 ```
 
 ## Inspiration
+[Images]((https://developers.google.com/web/fundamentals/design-and-ux/responsive/images))
 [Lazy Loading Images](https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/)<br>
 [Udacity course - Responsive Images](https://www.udacity.com/course/responsive-images--ud882)
 
 ## Documentation
-[HTML5 Embedded content](https://www.w3.org/TR/html5/semantics-embedded-content.htm)
+[HTML5 Embedded content](https://www.w3.org/TR/html5/semantics-embedded-content.htm)<br>
+[MDN Responsive images](https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images)
 
 ## License
 [MIT](LICENSE) 
--- src/components/app.js
+++ src/components/app.js
@@ -1,29 +1,33 @@
 import React from 'react'
 
-const url = [
-  'http://localhost:3001/photo-1534430480872-3498386e7856?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-  'http://localhost:3001/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-  'http://localhost:3001/photo-1479779978657-f97923f70d04?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-  'http://localhost:3001/photo-1495542779398-9fec7dc7986c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-  'http://localhost:3001/photo-1523509433743-6f42a58221df?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-  'http://localhost:3001/photo-1550301058-c64e2d2067d3?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-  'http://localhost:3001/photo-1529225596519-1262d0bfc242?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-  'http://localhost:3001/photo-1549925861-c14d41750718?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
+const url1 = [
+  'https://test.lukichev.pro/photo-1534430480872-3498386e7856?q=80&w=',
+  'https://test.lukichev.pro/photo-1496442226666-8d4d0e62e6e9?q=80&w=',
+  'https://test.lukichev.pro/photo-1479779978657-f97923f70d04?q=80&w=',
+  'https://test.lukichev.pro/photo-1495542779398-9fec7dc7986c?q=80&w=',
+  'https://test.lukichev.pro/photo-1523509433743-6f42a58221df?q=80&w=',
+  'https://test.lukichev.pro/photo-1529225596519-1262d0bfc242?q=80&w=',
+  'https://test.lukichev.pro/photo-1549925861-c14d41750718?q=80&w='
 ]
 
 
 const ImgLazyload = () => {
   return (
     <div>
-      <img src={url[0]} />
-      <img src={url[1]} />
-      <img src={url[2]} />
-      <img src={url[3]} />
-      <img src={url[4]} />
-      <img src={url[5]} />
-      <img src={url[6]} />
-      <img src={url[7]} />
-      <img src={url[8]} />
+      <img
+        data-src={`${url1[0]}1000`}
+        data-srcSet={`
+          ${url1[0]}300 300w,
+          ${url1[1]}500 500w,
+          ${url1[2]}600 600w,
+          ${url1[3]}800 800w,
+          ${url1[4]}1000 1000w,
+          ${url1[5]}1200 1200w,
+          ${url1[6]}1500 1500w,
+        `}
+        data-sizes={`90vw`}
+        className='lazyload'
+      />
     </div>
   )
 }
